dbc0d565c93939c5d75108d6c6812fdea11555b2
GuilhermeRS11 <guilhermeribeiro201342@gmail.com>
2023-04-14 10:16:48 -0300

    Finalizacao RDM e Atualizacao designe

--- development/RDM.py
+++ development/RDM.py
@@ -1,3 +1,4 @@
+
 import rdm_define as rdm
 import sys
 
@@ -10,21 +11,21 @@ def Calc_checksum(frame_size, frame):
 def Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD):
     # Organiza o frame de acordo com os parâmetros passados e no formato do RDM
     frame = []
-    frame.append(rdm.E120_SC_RDM)                  # Start Code (SC)
+    frame.append(rdm.E120_SC_RDM)                 # Start Code (SC)
     frame.append(rdm.E120_SC_SUB_MESSAGE)         # Sub-Start Code
     frame.append(Message_Length)                  # Message Length
-    frame.append((UID_D >> 40) & 0xff)             # Destination UID bit 5
-    frame.append((UID_D >> 32) & 0xff)             # Destination UID bit 4
+    frame.append((UID_D >> 40) & 0xff)            # Destination UID bit 5
+    frame.append((UID_D >> 32) & 0xff)            # Destination UID bit 4
     frame.append((UID_D >> 24) & 0xff)            # Destination UID bit 3
-    frame.append((UID_D >> 16) & 0xff)             # Destination UID bit 2
-    frame.append((UID_D >> 8) & 0xff)              # Destination UID bit 1
-    frame.append(UID_D & 0xff)                     # Destination UID bit 0
-    frame.append((UID_S >> 40) & 0xff)             # Source UID bit 5
+    frame.append((UID_D >> 16) & 0xff)            # Destination UID bit 2
+    frame.append((UID_D >> 8) & 0xff)             # Destination UID bit 1
+    frame.append(UID_D & 0xff)                    # Destination UID bit 0
+    frame.append((UID_S >> 40) & 0xff)            # Source UID bit 5
     frame.append((UID_S >> 32) & 0xff)            # Source UID bit 4
     frame.append((UID_S >> 24) & 0xff)            # Source UID bit 3
-    frame.append((UID_S >> 16) & 0xff)           # Source UID bit 2
+    frame.append((UID_S >> 16) & 0xff)            # Source UID bit 2
     frame.append((UID_S >> 8) & 0xff)             # Source UID bit 1
-    frame.append(UID_S & 0xff)                   # Source UID bit 0
+    frame.append(UID_S & 0xff)                    # Source UID bit 0
     frame.append(TN)                              # Transaction Number
     frame.append(Port_ID)
     frame.append(Message_Count)
@@ -35,10 +36,214 @@ def Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Devi
     frame.append(PID & 0xff)                      # Lower_PID
     frame.append(PDL)
 
+    if (PDL == 12):                               # Frames que tem 12 bytes de UID (lower and upper bound) Disc_unique_branch
+        frame.append((LB_PD >> 40) & 0xff)        # Lower Bound UID bit 5
+        frame.append((LB_PD >> 32) & 0xff)        # Lower Bound UID bit 4
+        frame.append((LB_PD >> 24) & 0xff)        # Lower Bound UID bit 3
+        frame.append((LB_PD >> 16) & 0xff)        # Lower Bound UID bit 2
+        frame.append((LB_PD >> 8) & 0xff)         # Lower Bound UID bit 1
+        frame.append(LB_PD & 0xff)                # Lower Bound UID bit 0
+        frame.append((UB_PD >> 40) & 0xff)        # Upper Bound UID bit 5
+        frame.append((UB_PD >> 32) & 0xff)        # Upper Bound UID bit 4
+        frame.append((UB_PD >> 24) & 0xff)        # Upper Bound UID bit 3
+        frame.append((UB_PD >> 16) & 0xff)        # Upper Bound UID bit 2
+        frame.append((UB_PD >> 8) & 0xff)         # Upper Bound UID bit 1
+        frame.append(UB_PD & 0xff)                # Upper Bound UID bit 0
+
+        checksum = Calc_checksum(36, frame)
+        frame.append((checksum >> 8) & 0xff)      # Checksum high
+        frame.append(checksum & 0xff)             # Checksum low
+
+    elif (PDL == 2):                              # Para o Get_parameter_description
+        frame = (LB_PD >> 8) & 0xff
+        frame = LB_PD & 0xff
+
+        checksum = Calc_checksum(26, frame)
+        frame.append((checksum >> 8) & 0xff)
+        frame.append(checksum & 0xff)
+
+    elif (PDL == 1):                              # Para SET_identify_device
+        frame.append(LB_PD & 0xff)
+
+        checksum = Calc_checksum(25, frame)
+        frame.append((checksum >> 8) & 0xff)
+        frame.append(checksum & 0xff)
+
+    else:                                         # Para funcões sem PD (disc_mute, disc_un_mute etc.)
+        checksum = Calc_checksum(24, frame)
+        frame.append((checksum >> 8) & 0xff)
+        frame.append(checksum & 0xff)
+
     return frame
 
-data = Set_frame(0x01AC34DC1134,0x02ACDF436576,0x03,0x04,0x05,0x06,0x07AB,0x08,0x09AB,0x10,0x11,0x12)
+def DISC_unique_branch(UID_D, UID_S, TN, Port_ID, LB_PD, UB_PD):
+
+    Message_Length = 0x24  # 36 bytes
+    Message_Count = 0x00
+    Sub_Device = 0x0000
+    CC = rdm.E120_DISCOVERY_COMMAND
+    PID = rdm.E120_DISC_UNIQUE_BRANCH
+    PDL = 0X0c
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def DISC_mute(UID_D, UID_S, TN, Port_ID):
+
+    Message_Length = 0x18 # 24 bytes
+    Message_Count = 0x00
+    Sub_Device = 0x0000
+    CC = rdm.E120_DISCOVERY_COMMAND
+    PID = rdm.E120_DISC_MUTE
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def DISC_un_mute(UID_D, UID_S, TN, Port_ID):
+
+    Message_Length = 0x18  # 24 bytes
+    Message_Count = 0x00
+    Sub_Device = 0x0000
+    CC = rdm.E120_DISCOVERY_COMMAND
+    PID = rdm.E120_DISC_UN_MUTE
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_supported_parameters(UID_D, UID_S, TN, Port_ID, Sub_Device):
+
+    Message_Length = 0x18      # 24 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_SUPPORTED_PARAMETERS
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_parameter_description(UID_D, UID_S, TN, Port_ID, PID_Requested):
+
+    Message_Length = 0x1A      # 26 bytes
+    Message_Count = 0x00
+    Sub_Device = 0x0000
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_PARAMETER_DESCRIPTION
+    PDL = 0X02
+    LB_PD = PID_Requested
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_device_info(UID_D, UID_S, TN, Port_ID, Sub_Device):
+
+    Message_Length = 0x18              # 24 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_DEVICE_INFO
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_software_version_label(UID_D, UID_S, TN, Port_ID, Sub_Device):
+
+    Message_Length = 0x18     # 24 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_SOFTWARE_VERSION_LABEL
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_dmx_start_address(UID_D, UID_S, TN, Port_ID, Sub_Device):
+
+    Message_Length = 0x18      # 24 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_DMX_START_ADDRESS
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def GET_identify_device(UID_D, UID_S, TN,  Port_ID, Sub_Device):
+
+    Message_Length = 0x18      # 24 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_GET_COMMAND
+    PID = rdm.E120_IDENTIFY_DEVICE
+    PDL = 0X00
+    LB_PD = None
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def SET_dmx_start_address(UID_D, UID_S, TN, Port_ID, Sub_Device, DMX_address):
+
+    Message_Length = 0x1A          # 26 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_SET_COMMAND
+    PID = rdm.E120_DMX_START_ADDRESS
+    PDL = 0X02
+    LB_PD = DMX_address
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+def SET_identify_device(UID_D, UID_S, TN, Port_ID, Sub_Device, Identify_start_stop):
+
+    Message_Length = 0x19      # 25 bytes
+    Message_Count = 0x00
+    CC = rdm.E120_SET_COMMAND
+    PID = rdm.E120_IDENTIFY_DEVICE
+    PDL = 0X01
+    LB_PD = Identify_start_stop
+    UB_PD = None
+
+    data_set = Set_frame(UID_D, UID_S, TN, Message_Length, Port_ID, Message_Count, Sub_Device, CC, PID, PDL, LB_PD, UB_PD)
+    return data_set
+
+"""
+TN = 0x00
+ID = 0x01
+Identify_start_stop = 0x01
+PID_Resquested = 0x25FA
+DMX_address = 0xC13F
+Sub_Dev = 0x2AF7
+LB_PD = 0xFF2354E13DA2
+UB_PD = 0x40028922FEDC
+UID_D = 0x123456789ABC
+UID_S = 0xCBA987654321
+
+data_test = SET_identify_device(UID_D, UID_S, TN, ID, Sub_Dev, Identify_start_stop)
+frame_size = data_test[2] + 2 # A posição 2 guarda o tamanho do frame, com exceção dos dois bytes do checksum
+
 
 sys.stdout.write("0x")
-for i in range(23):
-    sys.stdout.write(hex(data[i])[2:])
+for i in range(frame_size):
+    hex_data = data_test[i].to_bytes(1, byteorder='little')
+    hex_string = hex_data.hex()
+    print(hex_string)
+    #sys.stdout.write(hex(hex_string[i])[2:])
+
+print("")
+print(frame_size)
+"""
--- development/rdm_dmx_Master.py
+++ development/rdm_dmx_Master.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import (QWidget, QMainWindow)
 from RDM_DMX_Master_ui import Ui_MainWindow
+import RDM
+from ast import literal_eval
 
 class RDM_DMX_Master(QWidget, Ui_MainWindow):
     def __init__(self, app):
@@ -7,25 +9,33 @@ class RDM_DMX_Master(QWidget, Ui_MainWindow):
         self.setupUi(self)
         self.app = app
         self.setWindowTitle("RDM DMX Master")
-        
 
         self.classe.activated.connect(self.ChangeParameter)
         self.send_command.clicked.connect(self.SendCommand)
         self.menuSair.triggered.connect(self.quit)
+
+        # Atualização dos bytes das caixas de exibição
+        self.destination_UID.textEdited.connect(self.caixaCommand)
+        self.source_UID.textEdited.connect(self.caixaCommand)
+        self.sub_device.textEdited.connect(self.caixaCommand)
+        self.port_ID.textEdited.connect(self.caixaCommand)
 
     def ChangeParameter(self):
         classe = self.classe.currentText()
         if(classe == "DISC"):
             self.parametro.clear()
             self.parametro.addItems(["Unique Branch","Mute","Unmute"])
+            self.additional_parameter.setEnabled(False) # Faz com que o parametro adicional não esteja disponivel para edição
                                    
         elif(classe == "GET"):
             self.parametro.clear()
             self.parametro.addItems(["Supported parameters","Parameter description","Device info", "Software version label", "DMX start address", "Identify device"])
+            self.additional_parameter.setEnabled(False) 
 
-        else:
+        else:   # Então é comando SET
             self.parametro.clear()
             self.parametro.addItems(["DMX start address","Identify device"])
+            self.additional_parameter.setEnabled(True) 
 
     def SendCommand(self):
         classe = self.classe.currentText()
@@ -41,3 +51,69 @@ class RDM_DMX_Master(QWidget, Ui_MainWindow):
 
     def quit(self):
         self.app.quit()
+
+    def caixaCommand(self):
+        classe = self.classe.currentText()
+        parameter = self.parametro.currentText()
+        destination_UID = "0x" + (self.destination_UID.displayText())
+        print(destination_UID)
+        source_UID = "0x" + self.source_UID.displayText()
+        sub_device = "0x" + self.sub_device.displayText()
+        port_id = "0x" + self.port_ID.displayText()
+        additional_parameter = "0x" + self.additional_parameter.displayText()
+
+        if not destination_UID:
+            destination_UID = 0x0
+        if not source_UID:
+            source_UID = 0x0
+        if not sub_device:
+            sub_device = 0x0
+        if not port_id:
+            port_id = 0x0
+        if not additional_parameter:
+            additional_parameter = 0x0
+            
+        destination_UID = literal_eval(destination_UID)
+        print(type(destination_UID))
+        print(destination_UID)
+        source_UID = literal_eval(source_UID)
+        sub_device = literal_eval(sub_device)
+        port_id = literal_eval(port_id)
+        additional_parameter = literal_eval(additional_parameter)
+
+        if(classe == "SET"):
+            if(parameter == "DMX start address"):
+                command2send = RDM.SET_dmx_start_address(destination_UID, source_UID, 0, port_id, sub_device, additional_parameter)
+            
+            else: # Então é Identify device
+                command2send = RDM.SET_identify_device(destination_UID, source_UID, 0, port_id, sub_device, additional_parameter)
+        
+        elif(classe == "GET"):
+            if(parameter == "Supported parameters"):
+                command2send = RDM.GET_supported_parameters(destination_UID, source_UID, 0, port_id, sub_device)
+
+            elif(parameter == "Parameter description"):
+                command2send = RDM.GET_parameter_description(destination_UID, source_UID, 0, port_id, additional_parameter)
+
+            elif(parameter == "Device info"):
+                command2send = RDM.GET_device_info(destination_UID, source_UID, 0, port_id, sub_device)
+
+            elif(parameter == "Software version label"):
+                command2send = RDM.GET_software_version_label(destination_UID, source_UID, 0, port_id, sub_device)
+
+            elif(parameter == "DMX start address"):
+                command2send = RDM.GET_dmx_start_address(destination_UID, source_UID, 0, port_id, sub_device)
+
+            else:                       #(parameter == "Identify device"):
+                command2send = RDM.GET_identify_device(destination_UID, source_UID, 0, port_id, sub_device)
+        
+        else:   # Classe DISC
+            if(parameter == "Unique Branch"):
+                command2send = RDM.DISC_unique_branch(destination_UID, source_UID, 0, port_id, additional_parameter)
+            elif(parameter == "Mute"):
+                command2send = RDM.DISC_mute(destination_UID, source_UID, 0, port_id)
+            else:                       # (parameter == "Unmute"):
+                command2send = RDM.DISC_un_mute(destination_UID, source_UID, 0, port_id)
+
+        #self.slave_Response.setText(command2send)
+        self.command_1.setText(hex(command2send[0]))
